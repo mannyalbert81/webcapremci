0a3a36297bcebc379db7e37de6e73bfa48ab4d22
Usuario <Usuario@192.168.1.121>
2018-03-29 08:15:59 -0500

    dsfdsfdf

--- webservices/LoginService.php
+++ webservices/LoginService.php
@@ -19,7 +19,6 @@
 			if(!empty($resultado)){
 				// existe el usuario va lleno.
 				
-				
 					$listUsr = [];
 				
 					foreach ($resultado as $res)
@@ -35,8 +34,6 @@
 						$rowfoto->fotografia_usuarios=base64_encode(pg_unescape_bytea($res->fotografia_usuarios));//$res->foto_fichas_fotos;
 						$listUsr[]=$rowfoto;
 					}
-				
-				
 				
 					echo json_encode($listUsr);
 				die();
@@ -57,10 +54,104 @@
 		
 		
 	}
-	else{
+	
+	
+	
+	
+	
+	if($accion=="banner"){
 		
-		echo "No podemos establecer conexión, intentelo mas tarde.";
-		die();
+		
+		$columnas = "id_publicidad_movil, id_usuarios, imagen_baner";
+		$tablas   = "publicidad_movil";
+		$where    = "1=1";
+		$id       = "id_publicidad_movil";
+		$resultSet = $db->getCondiciones($columnas ,$tablas ,$where, $id);
+		
+		
+		
+		$i=count($resultSet);
+		
+		$html="";
+		if($i>0)
+		{
+		
+		
+			$html .= "<div  class='col-xs-12 col-md-12 col-lg-12'>";
+			$html .= "<div class='col-xs-12 col-md-4 col-lg-4'>";
+			$html .= "</div>";
+			$html .= "<div class='col-xs-12 col-md-3 col-lg-3'>";
+			$html .= "<div id='myCarousel2' class='carousel slide' data-ride='carousel'>";
+			$html .= "<ol class='carousel-indicators'>";
+			$html .= "<li data-target='#myCarousel1' data-slide-to='0' class='active'></li>";
+			$html .= "<li data-target='#myCarousel2' data-slide-to='0' ></li>";
+			$html .= "</ol>";
+				
+			$html .= "<div class='carousel-inner' role='listbox'>";
+				
+			if(!empty($resultSet)){
+		
+				$numero=0;
+				foreach ($resultSet as $res){
+						
+					$numero++;
+					
+						
+					if($numero==1){
+		
+						$foto=base64_encode(pg_unescape_bytea($res->imagen_baner));
+						$imgficha = 'data:image/png;base64,'.$foto;
+						
+						$html .= "<div class='item active'>";
+						$html.='<img src="'.$imgficha.'" >';
+						$html .= "</div>";
+		
+					}else{
+						$foto=base64_encode(pg_unescape_bytea($res->imagen_baner));
+						$imgficha = 'data:image/png;base64,'.$foto;
+						
+						$html .= "<div class='item'>";
+						$html.='<img src="'.$imgficha.'" >';
+						$html .= "</div>";
+		
+					}
+					$foto="";
+					$imgficha="";
+				}
+		
+		
+			}
+				
+				
+		
+				
+				
+			$html .= "<a class='left carousel-control' href='#myCarousel2' role='button' data-slide='prev'>";
+			$html .= "<span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>";
+			$html .= "<span class='sr-only'>Previous</span>";
+			$html .= "</a>";
+			$html .= "<a class='right carousel-control' href='#myCarousel2' role='button' data-slide='next'>";
+			$html .= "<span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>";
+			$html .= "<span class='sr-only'>Next</span>";
+			$html .= "</a>";
+			$html .= "</div>";
+			$html .= "</div>";
+			$html .= "</div>";
+			$html .= "<div class='col-xs-12 col-md-4 col-lg-4'>";
+			$html .= "</div>";
+			$html .= "</div>";
+				
+				
+		
+		
+		}else{
+		
+			$html = "<b>Actualmente no hay publicidad registrada...</b>";
+		}
+		
+		
+		echo json_encode($html);
+		
 	}
 
 
