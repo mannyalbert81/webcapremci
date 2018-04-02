0753f13724394c22c730a1f6bf24b0aec2f54cf8
Usuario <Usuario@192.168.1.121>
2018-04-02 11:01:44 -0500

    sdfdsds

--- view/BienvenidaView.php
+++ view/BienvenidaView.php
@@ -46,7 +46,7 @@
   <body class="nav-md">
     <div class="container body">
       <div class="main_container">
-        <div class="col-md-3 left_col  menu_fixed">
+        <div class="col-md-3 left_col menu_fixed">
           <div class="left_col scroll-view">
             <?php include("view/modulos/logo.php"); ?>
 
